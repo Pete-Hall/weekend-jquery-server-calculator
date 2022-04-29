488c5af1b30ca59978f88bd02f7c50bcee306697
Pete Hall <pete.mack.hall@gmail.com>
2022-04-28 19:42:41 -0500

    Figured out how to capture the chosen operator and bundle all of the desired inputs into an object.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,24 +1,27 @@
 $(document).ready(onReady);
 
-let equations = [];
+//let equations = []; // for testing the object creation
+let chosenOperator = 0; // I wonder if this global variable will be a problem
 
 function onReady() {
   $('#equalsButton').on('click', equals);
   $('#operatorsIn').on('click', '.operatorChooseButton', operatorChoose);
 }
 
-function equals() {
+function equals() { // When the submit (`=` button) is clicked, capture this input (values and operator), bundle it up in an object, and send this object to the server via a POST.
   console.log('in equals');
   // get user input & place in an object
-  let newOperator = 0;
   let newEquation = {
-    firstNum: $('#firstNumIn'),
-    // operator
-    // lastNum
+    firstNum: $('#firstNumIn').val(),
+    operator: chosenOperator,
+    lastNum: $('#secondNumIn').val()
   }
+  //equations.push(newEquation); // for testing the object creation
 }
 
-function operatorChoose() {
+function operatorChoose() { // updates the chosenOperator for the current equation
   console.log('in operatorChoose');
-  console.log($(this).text());
+  console.log($(this).text()); // get the text content of the button element. https://api.jquery.com/val/ and https://api.jquery.com/text/
+  chosenOperator = $(this).text();
+  //console.log(chosenOperator);
 }
